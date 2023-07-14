1a41192ed6c4f1fe30847fb30a47bf4eb20f01f7
Alejandro Moreira <moreiramikel11@gmail.com>
2023-07-13 22:26:12 -0500

    Array Vehiculos

---
+++ src/src/Vehiculoss.java
@@ -0,0 +1,122 @@
+package src.src;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Vehiculoss {
+    private JTextField marcaT;
+    private JTextField anioT;
+    private JTextField cilinT;
+    private JButton guardarButton;
+    private JButton leftButton;
+    private JButton rightButton;
+    private JPanel Vehi;
+    private JLabel marcaL;
+    private JLabel anioL;
+    private JLabel cilindrajeL;
+    private JLabel respuestaL;
+    private JButton cargarButton;
+    private JButton cleanButton;
+
+    private ArrayList carrosM = new ArrayList();
+    private ArrayList carrosA = new ArrayList();
+    private ArrayList carrosC = new ArrayList();
+    private boolean car=false;
+    private int i=0;
+
+    public Vehiculoss() {
+        guardarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                carrosM.add(marcaT.getText());
+                carrosA.add(anioT.getText());
+                carrosC.add(cilinT.getText());
+                marcaT.setText("");
+                anioT.setText("");
+                cilinT.setText("");
+                respuestaL.setText("Se a guardado el auto correctamente");
+            }
+        });
+
+        cargarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                car=true;
+                if (carrosM.size() == 0){
+                    respuestaL.setText("No existen datos");
+                }
+                else{
+                    marcaT.setText((String) carrosM.get(0));
+                    anioT.setText((String) carrosA.get(0));
+                    cilinT.setText((String) carrosC.get(0));
+                }
+            }
+        });
+        leftButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int aux = carrosM.size();
+                if (car){
+                    if (i < aux-1){
+                        respuestaL.setText("");
+                        i = i+1;
+                        marcaT.setText((String) carrosM.get(i));
+                        anioT.setText((String) carrosA.get(i));
+                        cilinT.setText((String) carrosC.get(i));
+                    }
+                    else {
+                        respuestaL.setText("No existen mas elementos");
+                    }
+                }
+                else{
+                    respuestaL.setText("Cargue los datos primero");
+                }
+            }
+        });
+        rightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (car) {
+                    if (i > 0) {
+                        i = i - 1;
+                        respuestaL.setText("");
+                        marcaT.setText((String) carrosM.get(i));
+                        anioT.setText((String) carrosA.get(i));
+                        cilinT.setText((String) carrosC.get(i));
+                    } else {
+                        respuestaL.setText("No existen mas elementos");
+                    }
+                }
+                else{
+                    respuestaL.setText("Cargue los datos primero");
+                }
+            }
+        });
+        cleanButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(car){
+                    marcaT.setText("");
+                    anioT.setText("");
+                    cilinT.setText("");
+                    car=false;
+                }
+                else{
+                    marcaT.setText("");
+                    anioT.setText("");
+                    cilinT.setText("");
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Vehiculoss");
+        frame.setContentPane(new Vehiculoss().Vehi);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
